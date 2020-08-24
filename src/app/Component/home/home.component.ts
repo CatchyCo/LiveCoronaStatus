fca60f0440af2879a9648d5caf6338cedb3e69c0
CatchyCo <santosh.shinde.mca15@gmail.com>
2020-08-24 21:32:46 +0530

    Done with final data : now starting with semantic ng chart

--- src/app/Component/home/home.component.ts
+++ src/app/Component/home/home.component.ts
@@ -9,14 +9,25 @@ import { LiveDataServiceService } from 'src/app/Services/live-data-service.servi
 export class HomeComponent implements OnInit {
 
   constructor(public liveDataService: LiveDataServiceService) { }
+  totalConfirmCase = 0;
+  totalDeathCases = 0;
+  totalRecovered = 0;
+  totalActive = 0;
 
   ngOnInit(): void {
     this.liveDataService.getGlobalData()
       .subscribe({
         next: (result) => {
-          console.log(result);
+          result.forEach(country => {
+            if (country['countryName']) {
+              this.totalConfirmCase = this.totalConfirmCase + country['totalConfirmCase'];
+              this.totalDeathCases = this.totalDeathCases + country['totalDeath'];
+              this.totalRecovered = this.totalRecovered + country['totalRecover'];
+              this.totalActive = this.totalActive + country['active'];
+            }
+          })
         }
       });
-  }
+  } 
 
 }
--- src/app/Services/live-data-service.service.ts
+++ src/app/Services/live-data-service.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 import { GlobalData } from '../global-data';
+import { global } from '@angular/compiler/src/util';
 
 
 @Injectable({
@@ -9,12 +10,9 @@ import { GlobalData } from '../global-data';
 })
 export class LiveDataServiceService {
 
-
   globalDataURL = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/08-23-2020.csv';
-  finalData: GlobalData[] = [];
-
+  row = {};
   constructor(public http: HttpClient) { }
-
 
   getGlobalData() {
     return this.http.get(this.globalDataURL, { responseType: 'text' }).pipe(
@@ -22,32 +20,35 @@ export class LiveDataServiceService {
       map(result => {
         let globalData: GlobalData[] = [];
         const rows = result.split('\n');
+        let raw = {};
         rows.splice(0, 1);
+
         rows.forEach(row => {
           const col = row.split(/,(?=\S)/);
           if (parseInt(col[7]) != NaN) {
-            globalData.push({
+            let cs = {
               countryName: col[3],
               totalConfirmCase: +col[7],
               totalDeath: +col[8],
               totalRecover: +col[9],
               active: +col[9]
-            })
+            }
+            let temp: GlobalData = raw[cs.countryName];
+            if (temp) {
+              temp.active = cs.active + temp.active;
+              temp.totalConfirmCase = cs.totalConfirmCase + temp.totalConfirmCase;
+              temp.totalDeath = cs.totalDeath + temp.totalDeath;
+              temp.totalRecover = cs.totalRecover + temp.totalRecover;
+              raw[cs.countryName] = temp;
+              //  console.log(temp);
+            } else {
+              raw[cs.countryName] = cs;
+            }
           }
         });
-        this.getFinalData(globalData);
-        // return result;
+        return (Object.values(raw));
       })
     );
   }
-
-  getFinalData(globalData: GlobalData[]) {
-    globalData.forEach(data => (
-      console.log(data)
-    )
-
-    )
-  }
-
 
 }
