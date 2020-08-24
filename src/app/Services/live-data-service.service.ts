cba79861a7e948bdd3942dc80d6cee62535b8bfb
CatchyCo <santosh.shinde.mca15@gmail.com>
2020-08-24 18:42:38 +0530

    Data catching in varible; before final data

--- src/app/Services/live-data-service.service.ts
+++ src/app/Services/live-data-service.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
+import { GlobalData } from '../global-data';
+
 
 @Injectable({
   providedIn: 'root'
@@ -9,20 +11,43 @@ export class LiveDataServiceService {
 
 
   globalDataURL = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/08-23-2020.csv';
-  globalData;
+  finalData: GlobalData[] = [];
+
   constructor(public http: HttpClient) { }
+
 
   getGlobalData() {
     return this.http.get(this.globalDataURL, { responseType: 'text' }).pipe(
-      map(
-        (result) => {
 
-          let rows = result.split('\n');
-          rows = rows.splice(0);
-          console.log(rows[0]);
-          //  return result;
-        }
-      )
+      map(result => {
+        let globalData: GlobalData[] = [];
+        const rows = result.split('\n');
+        rows.splice(0, 1);
+        rows.forEach(row => {
+          const col = row.split(/,(?=\S)/);
+          if (parseInt(col[7]) != NaN) {
+            globalData.push({
+              countryName: col[3],
+              totalConfirmCase: +col[7],
+              totalDeath: +col[8],
+              totalRecover: +col[9],
+              active: +col[9]
+            })
+          }
+        });
+        this.getFinalData(globalData);
+        // return result;
+      })
     );
   }
+
+  getFinalData(globalData: GlobalData[]) {
+    globalData.forEach(data => (
+      console.log(data)
+    )
+
+    )
+  }
+
+
 }
---
+++ src/app/global-data.ts
@@ -0,0 +1,7 @@
+export interface GlobalData {
+    countryName?: string,
+    totalDeath?: number,
+    totalConfirmCase?: number,
+    totalRecover?: number,
+    active?: number
+}
