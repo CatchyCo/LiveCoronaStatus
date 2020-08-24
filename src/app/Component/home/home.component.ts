ab97d6ef992388b0544036e5d2cccb396f842d2c
CatchyCo <santosh.shinde.mca15@gmail.com>
2020-08-24 15:54:38 +0530

    url setup for Live data

--- src/app/Component/home/home.component.ts
+++ src/app/Component/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LiveDataServiceService } from 'src/app/Services/live-data-service.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(public liveDataService: LiveDataServiceService) { }
 
   ngOnInit(): void {
+    this.liveDataService.getGlobalData()
+      .subscribe({
+        next: (result) => {
+          console.log(result);
+        }
+      });
   }
 
 }
--- src/app/Services/live-data-service.service.ts
+++ src/app/Services/live-data-service.service.ts
@@ -1,10 +1,28 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LiveDataServiceService {
 
-  constructor(public liveDataFromServer: HttpClient) { }
+
+  globalDataURL = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/08-23-2020.csv';
+  globalData;
+  constructor(public http: HttpClient) { }
+
+  getGlobalData() {
+    return this.http.get(this.globalDataURL, { responseType: 'text' }).pipe(
+      map(
+        (result) => {
+
+          let rows = result.split('\n');
+          rows = rows.splice(0);
+          console.log(rows[0]);
+          //  return result;
+        }
+      )
+    );
+  }
 }
